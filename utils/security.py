e21414c9c0e8b9bd3efeafc1cd59fcd50254f2dd
PRASANTH SENTHIL <prasanthsenthilkumar09@gmail.com>
2026-06-11 16:35:21 +0530

    feat: Add show/hide password toggle and simplify password validation to min 8 chars

--- utils/security.py
+++ utils/security.py
@@ -14,19 +14,10 @@ def validate_password(password):
     """
     Validate password strength:
     - Min 8 characters
-    - At least 1 letter, 1 number, 1 special character
+    - Numbers and symbols are optional
     """
     if not password or len(password) < 8:
         return False
-    
-    # Must contain at least one letter, one number, and one special character
-    if not re.search(r'[A-Za-z]', password):
-        return False
-    if not re.search(r'[0-9]', password):
-        return False
-    if not re.search(r'[!@#$%^&*(),.?":{}|<>]', password):
-        return False
-        
     return True
 
 def validate_phone(phone):
